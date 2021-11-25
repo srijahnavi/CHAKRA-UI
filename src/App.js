80cfd0decc2e087782d541b767090c6c2e2d1ac8
srijahnavi <jahnavipachipala@gmail.com>
2021-11-25 11:04:02 +0530

    initial commit -dark and light themes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { IconButton } from "@chakra-ui/button";
+import { useColorMode } from "@chakra-ui/color-mode";
+import { Flex, VStack, Heading, Spacer } from "@chakra-ui/layout";
+import { FaSun, FaMoon } from "react-icons/fa";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const { colorMode, toggleColorMode } = useColorMode();
+const isDark = colorMode === "dark";
+return (
+	<VStack>
+	<Flex w="100%">
+		<Heading ml="2" size="md" fontWeight='extrabold'
+		color='blue.500' >GGRestro</Heading>
+		<Spacer></Spacer>
+	<IconButton ml={9} icon={isDark ? <FaSun /> : <FaMoon />}
+	isRound="true" onClick={toggleColorMode}></IconButton>
+	</Flex>
+	</VStack>
+);
 }
 
 export default App;
